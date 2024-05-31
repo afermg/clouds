11735b337ec8d00d95d6d4631ba4f70fcfa2cec6
Alan Munoz <afer.mg@gmail.com>
2024-05-31 11:56:24 -0400

    add email/username for llanos

--- homes/llanos/packages.nix
+++ homes/llanos/packages.nix
@@ -4,15 +4,8 @@
 
   programs.git = {
     enable = true;
-    userName = "Alan Munoz";
-    userEmail = "afer.mg@gmail.com";
-    #extraConfig = {
-        # Sign all commits using ssh key
-    #    commit.gpgsign = true;
-    #    gpg.format = "ssh";
-    #    gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-    #    user.signingkey = "~/.ssh/id_ed25519.pub";
-    #  };
+    userName = "PaulaLlanos";
+    userEmail = "llanos.paula@gmail.com";
   };
 
   programs.direnv = {
